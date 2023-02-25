97824eba91ef227e2fb61b0e2d6745f2317e5d9e
Jaesuk <jaesuk95@outlook.com>
2023-02-25 20:49:16 +0900

    django restful get 요청

---
+++ order/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from order.models import Shop
+from order.serializer import ShopSerializer
+
+
+def shop(request):
+    if request.method == 'GET':
+        # snippets = Snippet.objects.all()    # Snippet 은 데이터 베이스 이름이다
+        # serializer = SnippetSerializer(snippets, many=True)
+        shop = Shop.objects.all()
+        serializer = ShopSerializer(shop,many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    # elif request.method == 'POST':
+
+# Create your views here.
