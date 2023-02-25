a4207ace0535086e047a4d6ac06f2195d47105c7
Jaesuk <jaesuk95@outlook.com>
2023-02-25 21:26:53 +0900

    메뉴 등록

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from order import views
 
 urlpatterns = [
-    path('shops/', views.shop)
+    path('shops/', views.shop),
+    path('menu/', views.menu)
 ]
--- order/views.py
+++ order/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
-from order.models import Shop
-from order.serializer import ShopSerializer
+from order.models import Shop, Menu
+from order.serializer import ShopSerializer, MenuSerializer
 
 
-@csrf_exempt    # csrf = 보안적인, 프론트와 백에서 데이터를 바꿔치기를 방지한다
+@csrf_exempt  # csrf = 보안적인, 프론트와 백에서 데이터를 바꿔치기를 방지한다
 def shop(request):
     if request.method == 'GET':
         # snippets = Snippet.objects.all()    # Snippet 은 데이터 베이스 이름이다
         # serializer = SnippetSerializer(snippets, many=True)
         shop = Shop.objects.all()
-        serializer = ShopSerializer(shop, many=True)
+        serializer = ShopSerializer(shop, many=True)  # many=True 데이터가 여러개 있어도 상관 안한다
         return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
@@ -24,4 +24,20 @@ def shop(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-# Create your views here.
+
+@csrf_exempt
+def menu(request):
+    if request.method == 'GET':
+        # snippets = Snippet.objects.all()    # Snippet 은 데이터 베이스 이름이다
+        # serializer = SnippetSerializer(snippets, many=True)
+        menu = Menu.objects.all()
+        serializer = MenuSerializer(menu, many=True)  # many=True 데이터가 여러개 있어도 상관 안한다
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = MenuSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
